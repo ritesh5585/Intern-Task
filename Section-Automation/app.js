dc0a5720fb08c8159dd29a1833b346fe726f4cd8
Ritesh <vishwakarmaritesh5585@gmail.com>
2026-07-31 12:17:53 +0530

    feat: streamline app.js by reorganizing middleware and route definitions

--- Section-Automation/app.js
+++ Section-Automation/app.js
@@ -1,47 +1,32 @@
 const express = require("express");
-const path = require("path");
-
-const app = express();
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static(path.join(__dirname, "public")));
-
-// Routes
 const presentationRoutes = require("./routes/presentation.routes");
 const urlRoutes = require("./routes/url.routes");
 const urlController = require("./controllers/url.controller");
+const path = require("path");
+
+const app = express();
+const pptDir = path.join(__dirname, "../ppt-updated");
+
+app.use(express.json());
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/ppt-updated", express.static(pptDir));
+
+app.set("views", path.join(__dirname, "views"));
 
 app.use("/presentation", presentationRoutes);
 app.use("/url", urlRoutes);
 
-// Automation home
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-// PPT folder
-const pptDir = path.join(__dirname, "../ppt-updated");
-
-// Serve PPT page with embedded user/company params
 app.get(
   "/ppt-updated/index.html/:username/:companyName",
   urlController.showPage,
 );
-app.get(
-  "/ppt-updated/index.html/:username/:companyName/*",
-  urlController.showPage,
-);
 
-// Serve PPT assets
-app.use("/ppt-updated", express.static(pptDir));
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server Running on ${PORT}`);
+app.listen(3000, () => {
+  console.log(`Server Running on http://localhost:3000`);
 });
